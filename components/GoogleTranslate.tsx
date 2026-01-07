2a2e29d00de6afdbad2830b0401b53fa515222d6
Vignesh <koyiladavignesh@gmail.com>
2026-01-07 12:37:55 +0530

    feat: integrate Google Translate component and update language switch logic

--- components/GoogleTranslate.tsx
+++ components/GoogleTranslate.tsx
@@ -1,45 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+if (typeof window !== "undefined" && typeof Node === "function") {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function <T extends Node>(child: T): T {
+    if (child.parentNode !== this) {
+      return child;
+    }
+    return originalRemoveChild.call(this, child) as T;
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function <T extends Node>(
+    newNode: T,
+    referenceNode: Node | null
+  ): T {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      return newNode;
+    }
+    return originalInsertBefore.call(this, newNode, referenceNode) as T;
+  };
+}
 
 export default function GoogleTranslateSafety() {
-  useEffect(() => {
-    // This code patches the browser's DOM methods to prevent React from crashing
-    // when Google Translate modifies the DOM (wrapping text in <font> tags).
-    // Source: https://github.com/facebook/react/issues/11538#issuecomment-417504600
-    // and https://martijnhols.nl/gists/everything-about-google-translate-crashing-react
-
-    if (typeof Node === 'function' && Node.prototype) {
-      const originalRemoveChild = Node.prototype.removeChild;
-      Node.prototype.removeChild = function <T extends Node>(child: T): T {
-        if (child.parentNode !== this) {
-          if (console) {
-            console.warn(
-              'Cannot remove a child from a different parent. Google Translate fix applied.'
-            );
-          }
-          return child;
-        }
-        return originalRemoveChild.call(this, child) as T;
-      };
-
-      const originalInsertBefore = Node.prototype.insertBefore;
-      Node.prototype.insertBefore = function <T extends Node>(
-        newNode: T,
-        referenceNode: Node | null
-      ): T {
-        if (referenceNode && referenceNode.parentNode !== this) {
-          if (console) {
-            console.warn(
-              'Cannot insert before a reference node from a different parent. Google Translate fix applied.'
-            );
-          }
-          return newNode;
-        }
-        return originalInsertBefore.call(this, newNode, referenceNode) as T;
-      };
-    }
-  }, []);
-
   return null;
 }
